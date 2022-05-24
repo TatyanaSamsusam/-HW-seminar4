4752b5817f7f0924d1877e2c6505607e26830152
Tatyana <android87@list.ru>
2022-05-24 14:05:01 +0400

    finds sum of digits

---
+++ task02_sumOfDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число, и я выдам сумму цифр в числе");
+int number = int.Parse(Console.ReadLine());
+
+int FindSum (int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        int digit = num %10;
+        result = result + digit;
+        num = num /10;
+    }
+    return result;
+}
+
+int sum = FindSum(number);
+Console.WriteLine($"сумма цифр в числе {number} равна {sum}");
