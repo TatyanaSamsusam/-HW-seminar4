1a0ab60fe31ebe9316119b766b1b22b0be43849e
Tatyana <android87@list.ru>
2022-05-24 13:46:52 +0400

    made pow using cycle

---
+++ task01_natStepen/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите два числа (A и B), и я возведу число A в натуральную степень B");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+int FindPower(int a, int b)
+{
+    int powres = 1;
+    for (int i = 0  ; i < b; i++)
+    {
+        powres = powres * a;
+    }
+    return powres;
+}
+
+int result = FindPower(numberA, numberB);
+Console.WriteLine($"{numberA} в {numberB} степени равно {result}");
